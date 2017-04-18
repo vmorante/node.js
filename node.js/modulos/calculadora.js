d1486bb49dc1393ccaf24b1425b92f377e7f7bb4
vmorante <veronica.morante.pindado@gmail.com>
2017-04-18 11:24:08 +0200

    calculadora

---
+++ node.js/modulos/calculadora.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log('Inicializo la calculadora....')
+
+function suma(a, b) {
+    return a + b;
+}
+
+function resta(a, b) {
+    return a - b;
+}
+module.exports = {
+    suma: suma,
+    resta: resta
+
+};
---
+++ node.js/modulos/index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const calculadora = require('./calculadora');
+
+console.log(calculadora.suma(3, 8))
